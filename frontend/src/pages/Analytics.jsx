db1e341b6afb20f69870165e919b49855027302b
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-11-20 01:38:28 +0500

    Implement Analytics file.

--- frontend/src/pages/Analytics.jsx
+++ frontend/src/pages/Analytics.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
+
 export default function Analytics() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    api.get("analytics/").then(res => setData(res.data)).catch(err => console.error(err));
+  }, []);
+
   return (
-    <div className="p-10 text-3xl font-bold">
-      Analytics Page
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Category Analytics</h1>
+      <div style={{ width: "100%", height: 350 }}>
+        <ResponsiveContainer>
+          <BarChart data={data}>
+            <XAxis dataKey="category" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="count" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }
